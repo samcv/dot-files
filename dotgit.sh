1720ae325ea56f17b33939edd7b3f8abbd156870
Samantha McVey <samantham@posteo.net>
2016-09-02 00:48:41 -0700

    update dotgit.sh

--- dotgit.sh
+++ dotgit.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 FOLDER=~/dotgit
-cd ~/dotgit
-FOLDER=`pwd`
-mkdir -p ${FOLDER}
+cd ~/dotgit || exit
+FOLDER=$(pwd)
+mkdir -p "${FOLDER}"
 
 echo "Coping this script to ${FOLDER}"
-cp ~/bin/dotgit.sh ${FOLDER}
+cp ~/bin/dotgit.sh "${FOLDER}"
 
 echo "Copying user dot files to ${FOLDER}"
-cp ~/.zshrc.local ${FOLDER}/_.zshrc.local
-cp ~/.xinitrc ${FOLDER}/_.xinitrc
+cp ~/.zshrc.local "${FOLDER}/_.zshrc.local"
+cp ~/.xinitrc "${FOLDER}/_.xinitrc"
 
-echo "Coping user ~/.config files to ${folder}/_.config"
-mkdir -p ${FOLDER}/_.config/mpd
-cp ~/.config/mpd/mpd.conf ${FOLDER}/_.config/mpd
+echo "Coping user ~/.config files to ${FOLDER}/_.config"
+mkdir -p "${FOLDER}/_.config/mpd"
+cp ~/.config/mpd/mpd.conf "${FOLDER}/_.config/mpd"
 
 echo "Copying /etc files to ${FOLDER}/etc"
-mkdir -p ${FOLDER}/etc
-mkdir -p ${FOLDER}/etc/connman
-cp /etc/connman/main.conf ${FOLDER}/etc/connman
-mkdir -p ${FOLDER}/etc/
-cp /etc/pacman.conf ${FOLDER}/etc
-cp /etc/makepkg.conf ${FOLDER}/etc
+mkdir -p "${FOLDER}/etc"
+mkdir -p "${FOLDER}/etc/connman"
+cp /etc/connman/main.conf "${FOLDER}/etc/connman"
+mkdir -p "${FOLDER}/etc/"
+cp /etc/pacman.conf "${FOLDER}/etc"
+cp /etc/makepkg.conf "${FOLDER}/etc"
 
 
 echo "Copying /var/lib files to ${FOLDER}/var/lib"
-mkdir -p ${FOLDER}/var/lib/
-mkdir -p ${FOLDER}/var/lib/connman
-sudo cp /var/lib/connman/settings ${FOLDER}/var/lib/connman
-sudo chown `whoami`:root ${FOLDER}/var/lib/connman/settings
+mkdir -p "${FOLDER}/var/lib/"
+mkdir -p "${FOLDER}/var/lib/connman"
+sudo cp /var/lib/connman/settings "${FOLDER}/var/lib/connman"
+sudo chown "$(whoami):root" "${FOLDER}/var/lib/connman/settings"
 echo "Copying /usr/share/devtools files to ${FOLDER}/usr/share/"
-mkdir -p ${FOLDER}/usr/share/devtools/
-cp /usr/share/devtools/pacman-extra.conf ${FOLDER}/usr/share/devtools/
+mkdir -p "${FOLDER}/usr/share/devtools/"
+cp /usr/share/devtools/pacman-extra.conf "${FOLDER}/usr/share/devtools/"
