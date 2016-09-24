682fbae55c281468613930aa21af8dac8eb6409a
Samantha McVey <samantham@posteo.net>
2016-09-24 01:57:00 -0700

    add dnsmasq.conf and connman.service

--- dotgit.sh
+++ dotgit.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env sh
+# Script to copy my dotfiles and other configuration files
+
 FOLDER=~/dotgit
 cd ~/dotgit || exit
 FOLDER=$(pwd)
@@ -8,10 +10,13 @@ echo "Coping this script to ${FOLDER}"
 cp ~/bin/dotgit.sh "${FOLDER}"
 
 echo "Copying user dot files to ${FOLDER}"
+# Copy anything in .zshrc.local that doesn't have a space in front of it
 sed -e 's/^ .*//' ~/.zshrc.local > "${FOLDER}/_.zshrc.local"
 cp ~/.xinitrc "${FOLDER}/_.xinitrc"
 cp ~/.Xresources "${FOLDER}/_.Xresources"
 cp ~/.vimrc "${FOLDER}/_.vimrc"
+
+echo "Copying atom configuration files"
 mkdir -p "${FOLDER}/_.atom"
 cp ~/.atom/keymap.cson "${FOLDER}/_.atom/"
 cp ~/.atom/styles.less "${FOLDER}/_.atom/"
@@ -25,6 +30,7 @@ echo "Copying /etc files to ${FOLDER}/etc"
 mkdir -p "${FOLDER}/etc"
 mkdir -p "${FOLDER}/etc/connman"
 cp /etc/connman/main.conf "${FOLDER}/etc/connman"
+cp /etc/dnsmasq.conf "${FOLDER}/etc"
 mkdir -p "${FOLDER}/etc/"
 cp /etc/pacman.conf "${FOLDER}/etc"
 cp /etc/makepkg.conf "${FOLDER}/etc"
@@ -33,6 +39,9 @@ cp /etc/makepkg.conf "${FOLDER}/etc"
 echo "Copying /var/lib files to ${FOLDER}/var/lib"
 mkdir -p "${FOLDER}/var/lib/"
 mkdir -p "${FOLDER}/var/lib/connman"
+echo "Copying /usr/lib/systemd/system files"
+mkdir -p "${FOLDER}/usr/lib/systemd/system"
+cp /usr/lib/systemd/system/connman.service "${FOLDER}/usr/lib/systemd/system"
 sudo cp /var/lib/connman/settings "${FOLDER}/var/lib/connman"
 sudo chown "$(whoami):root" "${FOLDER}/var/lib/connman/settings"
 echo "Copying /usr/share/devtools files to ${FOLDER}/usr/share/"
