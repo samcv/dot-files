83bffcf221f5993992a89da0ed8c6f16bbb56f91
Samantha McVey <samantham@posteo.net>
2016-10-19 01:26:08 -0700

    Add some comments to my profile.d files

--- etc/profile.d/compose.sh
+++ etc/profile.d/compose.sh
@@ -1,2 +1,5 @@
+# Make the compose key work globally in GTK and QT programs since not all programs
+# (especially GTK ones) follow the Xorg standards by default
+
 export GTK_IM_MODULE='xim'
 export QT_IM_MODULE='xim'
--- etc/profile.d/vdpau_driver.sh
+++ etc/profile.d/vdpau_driver.sh
@@ -1,2 +1,2 @@
-# Local script to set VDPAU_DRIVER to va_gl for Intel
+# Local script to set VDPAU_DRIVER to va_gl for Intel Graphics Cards
 export VDPAU_DRIVER=va_gl
