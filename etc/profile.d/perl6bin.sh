5c56720ec8d922a99000705de0a5b8b12aeb60c7
Samantha McVey <samantham@posteo.net>
2016-12-01 12:32:08 -0800

    Update Perl 6 Profile file

--- etc/profile.d/perl6bin.sh
+++ etc/profile.d/perl6bin.sh
@@ -1,15 +1,7 @@
 # Set path to perl6 scriptdirs if they exist
 
-#[ -d /usr/bin/site_perl ] && PATH=$PATH:/usr/bin/site_perl
-[ -d  /usr/share/perl6/site/bin ] && PATH=$PATH:/usr/share/perl6/site/bin
-
-[ -d /usr/share/perl6/vendor/bin ] && PATH=$PATH:/usr/share/perl6/vendor/bin
-[ -d /usr/share/perl6/bin ] && PATH=$PATH:/usr/share/perl6/bin
-
-#[ -d /usr/bin/core_perl ] && PATH=$PATH:/usr/bin/core_perl
+[ -d  /usr/share/perl6/site/bin ] && PATH="$PATH:/usr/share/perl6/site/bin"
+[ -d /usr/share/perl6/vendor/bin ] && PATH="$PATH:/usr/share/perl6/vendor/bin"
+[ -d /usr/share/perl6/bin ] && PATH="$PATH:/usr/share/perl6/bin"
 
 export PATH
-
-# If you have modules in non-standard directories you can add them here.
-#export PERLLIB=dir1:dir2
-
